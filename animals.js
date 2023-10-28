9d9b8f5ddab2959cf7409ca1dbe016a047ec7bae
Gabriel Fiore <gabriel_fiore@hotmail.com>
2023-10-28 11:11:49 -0300

    finalizado

---
+++ animals.js
@@ -0,0 +1,6 @@
+const animals = ["pinguim", "onça", "tigre", "cachorro", "leão"];
+if (!animals.includes("leão")) {
+    console.log("Leão não existe no array animals.");
+    } else {
+        console.log("Existe um leão no array animals.");
+    }
---
+++ series.js
@@ -0,0 +1,2 @@
+const my3FavoriteTVShows = ["La Casa de Papel", "The Walking Dead", "O Atirador"];
+console.log(my3FavoriteTVShows.length === 3);
